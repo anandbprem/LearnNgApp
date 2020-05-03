daa7684bf364ff1dbbfd2bd106fa197272bf0328
Anand B <anand.basavaraju@kdi.kongsberg.com>
2020-05-03 23:29:26 +0530

    Angular Pipes : formatting string, number, Currency, and percent Printing keyvalue pair - to check the actual values in debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,18 @@ export class AppComponent {
   title = 'LearnNgApp';
 
   courses = COURSES;
+  
+  // The below variables are used for learning pipes
+  startDate = new Date(2000, 0, 1);
+  titleCourse = COURSES[1].description;
+  price = 9.99234345;
+  // Percent pipe will multiply by 100 by default
+  rate =0.67;
+  // used to display the key value pairs 
+  firstCourse = COURSES[1];
 
-  onCourseSelected(course: Course){
-    console.log("App Component");
-    console.log("click Event Bubbled");
+  onCourseSelected(course: Course) {
+    console.log('App Component');
+    console.log('click Event Bubbled');
   }
 }
